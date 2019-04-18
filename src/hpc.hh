86ec8e1eea0696fcf1021de301674d00497a15d6
scam627 <stiven.cardona627@gmail.com>
2019-04-18 11:12:16 -0500

    hpc library update v 1.3

--- src/hpc.hh
+++ src/hpc.hh
@@ -18,6 +18,7 @@ class matrix
 
   public:
     //__Constructors__
+
     matrix(size_t sz) : s(sz)
     {
         srand(time(NULL));
@@ -33,17 +34,20 @@ class matrix
     }
 
     //__Operators__
+
     vector<T> &
     operator[](int i)
     {
         return storage[i];
     }
+
     void operator=(matrix<T> b)
     {
         for (int i = 0; i < s; i++)
             for (int j = 0; j < s; j++)
                 storage[i][j] = b[i][j];
     }
+
     matrix<T> operator*(matrix<T> b)
     {
         matrix<T> mat(s, 0);
@@ -55,6 +59,15 @@ class matrix
     }
 
     //__Methods__
+    void show()
+    {
+        for (auto v : storage)
+        {
+            for (auto i : v)
+                cout << i << " ";
+            cout << "\n";
+        }
+    }
 
     size_t size()
     {
@@ -81,6 +94,43 @@ void mult(matrix<T> &a, matrix<T> &b, matrix<T> &c)
     }
     for (auto &t : threads)
         t.join();
+}
+
+// pendiente crear una clase test que me permita generalizar los testings
+
+void parallel_test(vector<int> &sizes, int n)
+{
+    for (auto sz : sizes)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            clock_t begin_time = clock();
+            srand(time(0));
+            matrix<int> a(sz);
+            matrix<int> b(sz);
+            matrix<int> c(sz, 0);
+            mult(a, b, c);
+            clock_t end_time = clock();
+            cout << sz << " " << fixed << setprecision(6) << (end_time - begin_time) * 1.0 / CLOCKS_PER_SEC << "\n";
+        }
+    }
+}
+
+void serial_test(vector<int> &sizes, int n)
+{
+    for (auto sz : sizes)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            clock_t begin_time = clock();
+            srand(time(0));
+            matrix<int> a(sz);
+            matrix<int> b(sz);
+            matrix<int> c = a * b;
+            clock_t end_time = clock();
+            cout << sz << " " << fixed << setprecision(6) << (end_time - begin_time) * 1.0 / CLOCKS_PER_SEC << "\n";
+        }
+    }
 }
 
 #endif
